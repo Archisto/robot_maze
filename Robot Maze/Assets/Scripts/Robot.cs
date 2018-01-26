c6db69e2a5d08be646dab430cbaf689b9dfbc082
Lauri Johannes Leponiemi <lauri.leponiemi@cs.tamk.fi>
2018-01-27 01:31:51 +0200

    Robot can walk and turn

--- Robot Maze/Assets/Scripts/Robot.cs
+++ Robot Maze/Assets/Scripts/Robot.cs
@@ -6,6 +6,8 @@ namespace RobotMaze
 {
     public class Robot : MonoBehaviour
     {
+
+		bool canAct = true;
         /// <summary>
         /// Initializes the object.
         /// </summary>
@@ -19,7 +21,59 @@ namespace RobotMaze
         /// </summary>
         private void Update()
         {
-
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				MoveForward ();
+			} else if (Input.GetKeyDown (KeyCode.LeftControl)) {
+				RotateLeft ();
+			} else if (Input.GetKeyDown (KeyCode.LeftShift)) {
+				RotateRight ();
+			}
         }
+
+		public void MoveForward()
+		{
+			if (canAct) {
+				StartCoroutine (MoveSquare ());
+			}
+		}
+
+		public void RotateLeft(){
+			if (canAct) {
+				StartCoroutine (SpinLeft ());
+			}
+		}
+
+		public void RotateRight(){
+			if (canAct) {
+				StartCoroutine (SpinRight ());
+			}
+		}
+
+		IEnumerator MoveSquare(){
+			canAct = false;
+			for(int i = 0; i < 60; i++){
+				transform.Translate (Vector3.forward * 1 / 60);
+				yield return new WaitForSeconds (Time.deltaTime);
+			}
+			canAct = true;
+		}
+
+		IEnumerator SpinLeft(){
+			canAct = false;
+			for(int i = 0; i < 90; i++){
+				transform.Rotate (Vector3.down, 1f);
+				yield return new WaitForSeconds (Time.deltaTime);
+			}
+			canAct = true;
+		}
+
+		IEnumerator SpinRight(){
+			canAct = false;
+			for(int i = 0; i < 90; i++){
+				transform.Rotate (Vector3.down, -1f);
+				yield return new WaitForSeconds (Time.deltaTime);
+			}
+			canAct = true;
+		}
     }
 }
