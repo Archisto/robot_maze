ac74e05fd04f532ffd56cdc5834252033115bf09
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2018-01-26 21:34:13 +0200

    Added scripts

---
+++ Robot Maze/Assets/Scripts/Instruction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RobotMaze
+{
+    public enum InstructionType
+    {
+        MoveForward = 0,
+        TurnLeft = 1,
+        TurnRight = 2,
+        UTurn = 3,
+        PickUp = 4,
+        Drop = 5,
+        Stop = 6
+    }
+
+    public class Instruction : MonoBehaviour
+    {
+        public InstructionType type;
+        public string text;
+
+        public bool Active { get; set; }
+
+        /// <summary>
+        /// Initializes the object.
+        /// </summary>
+        private void Start()
+        {
+
+        }
+
+        /// <summary>
+        /// Update is called once per frame.
+        /// </summary>
+        private void Update()
+        {
+
+        }
+    }
+}
---
+++ Robot Maze/Assets/Scripts/InstructionList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RobotMaze
+{
+    public class InstructionList : MonoBehaviour
+    {
+        public Instruction[] instructions;
+
+        /// <summary>
+        /// Initializes the object.
+        /// </summary>
+        private void Start()
+        {
+            instructions = GetComponentsInChildren<Instruction>();
+        }
+
+        /// <summary>
+        /// Update is called once per frame.
+        /// </summary>
+        private void Update()
+        {
+
+        }
+    }
+}
