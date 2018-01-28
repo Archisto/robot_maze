2fe0a5d3f6f14584bce678c139c4a82c97831759
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2018-01-28 10:05:39 +0200

    Created a SpeedOption script

---
+++ Robot Maze/Assets/Scripts/SpeedOption.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RobotMaze
+{
+    public class SpeedOption : MonoBehaviour
+    {
+        [SerializeField]
+        private int number;
+
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+        }
+    }
+}
