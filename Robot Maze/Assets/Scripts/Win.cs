7dadc9c748e94a3cd541d06496ab7c1fad7fd088
Lauri Johannes Leponiemi <lauri.leponiemi@cs.tamk.fi>
2018-01-28 18:27:50 +0200

    Game works. Build. Gameplay footage.

--- Robot Maze/Assets/Scripts/Win.cs
+++ Robot Maze/Assets/Scripts/Win.cs
@@ -20,7 +20,7 @@ namespace RobotMaze
 			foreach (Robot rob in robots) {
 				rob.enabled = false;
 			}
-			SFXPlayer.Instance.Play (Sound.Chime, false);
+			SFXPlayer.Instance.Play (Sound.Bell, false);
 			_myParticleSystem.SetActive (true);
 		}
 	}
